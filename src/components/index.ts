3a7687a698e1a3a66cde7dbe22ce593aa8fa9daf
vabr@danskebank.lt <vabr@danskebank.lt>
2020-10-25 17:02:35 +0200

    default components export

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Layout } from './Layout';
+export { default as Link } from './Link';
+export { default as Navbar } from './Navbar';
+
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../components/Layout";
+import { Layout } from "../components";
 
 export default function Home() {
   return (

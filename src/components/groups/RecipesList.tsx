8d6335eb8035b3d995658cbd6e212ab473c86483
vabr@danskebank.lt <vabr@danskebank.lt>
2020-11-15 17:35:03 +0200

    prettier

--- src/components/groups/RecipesList.tsx
+++ src/components/groups/RecipesList.tsx
@@ -4,33 +4,33 @@ import { RecipeCard } from "../";
 import { RecipesListHardcoded } from "../../hardcodedData";
 
 const RecipesList = () => {
-    return (
-        <MainContainer>
-            <TitleContainer>
-                <h1>Receptai</h1>
-            </TitleContainer>
-            <List>
-                {RecipesListHardcoded.map((recipe) => (
-                    <RecipeCard key={recipe.slug} recipe={recipe} />
-                ))}
-            </List>
-        </MainContainer>
-    );
+  return (
+    <MainContainer>
+      <TitleContainer>
+        <h1>Receptai</h1>
+      </TitleContainer>
+      <List>
+        {RecipesListHardcoded.map((recipe) => (
+          <RecipeCard key={recipe.slug} recipe={recipe} />
+        ))}
+      </List>
+    </MainContainer>
+  );
 };
 
 const MainContainer = styled.main``;
 
 const TitleContainer = styled.div`
-    height: 6rem;
-    align-items: center;
-    justify-content: center;
+  height: 6rem;
+  align-items: center;
+  justify-content: center;
 `;
 
 const List = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default RecipesList;

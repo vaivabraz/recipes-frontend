0009905b801a8adf1a0beb7f22c3068bb351d4ce
vabr@danskebank.lt <vabr@danskebank.lt>
2020-12-26 21:25:13 +0200

    useFormValidation custom hook

---
+++ src/components/customHooks/useFormValidation.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+type valuesType = {
+  [key: string]: any;
+};
+
+function useFormValidation(
+  initialState: valuesType,
+  validate: (values: valuesType) => {},
+  submitFormAfterValidation: (values: valuesType) => void
+) {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  function handleChange(event) {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        submitFormAfterValidation(values);
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+  }
+
+  return {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    isSubmitting,
+  };
+}
+
+export default useFormValidation;

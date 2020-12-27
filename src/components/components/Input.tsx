72b95c8849dcfdd0b0e5088d1cdbf195b17b85bc
vabr@danskebank.lt <vabr@danskebank.lt>
2020-12-27 17:21:31 +0200

    added multiline prop to input component

--- src/components/components/Input.tsx
+++ src/components/components/Input.tsx
@@ -1,17 +1,27 @@
 import React, { InputHTMLAttributes } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Colors } from "../../constants";
 
 interface inputInterface extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: {};
+  multiline?: boolean;
 }
 
-const Input: React.FC<inputInterface> = ({ label, error, ...inputProps }) => {
+const Input: React.FC<inputInterface> = ({
+  label,
+  error,
+  multiline,
+  ...inputProps
+}) => {
   return (
     <InputContainer>
       {label && <Label>{label}</Label>}
-      <InputBox autoComplete="off" error={error} {...inputProps} />
+      {multiline ? (
+        <InputMultiline rows={4} error={error} {...inputProps} />
+      ) : (
+        <InputLine autoComplete="off" error={error} {...inputProps} />
+      )}
       {error && <Error>{error}</Error>}
     </InputContainer>
   );
@@ -37,10 +47,18 @@ const Error = styled.h5`
   color: ${Colors.errorsRed};
 `;
 
-const InputBox = styled.input`
+const InputStyle = css`
   display: flex;
   flex: 1;
   padding: 1rem;
   border: 1px solid;
   border-color: ${(props) => (props.error ? Colors.errorsRed : Colors.Border)};
+`;
+
+const InputLine = styled.input`
+  ${InputStyle}
+`;
+const InputMultiline = styled.textarea`
+  ${InputStyle}
+  resize: none;
 `;

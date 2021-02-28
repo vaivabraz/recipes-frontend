d22f772720dd9af1af40b6b429d3dc053ee213f9
vabr@danskebank.lt <vabr@danskebank.lt>
2021-02-28 22:20:13 +0200

    removed old code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,1 @@
-export { default as RecipesList } from "./groups/RecipesList";
-export { default as FullRecipeView } from "./groups/FullRecipeView";
 export { default as CreateNewRecipeView } from "./groups/CreateNewRecipeView";

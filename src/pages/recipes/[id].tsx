fc356f0fcbbab317b0b8dc2e45adc02fb78a326c
vabr@danskebank.lt <vabr@danskebank.lt>
2021-03-08 21:32:50 +0200

    .

--- src/pages/recipes/[id].tsx
+++ src/pages/recipes/[id].tsx
@@ -1,17 +1,18 @@
-import { FullRecipeView } from "../../components/";
 import { Layout } from "../../ui";
+import { RecipeView } from "../../pagesComponents";
 import { useRouter } from "next/router";
 import { FullRecipeType } from "../../types";
 import { userRecipeHardcoded } from "../../hardcodedData";
+import { userRecipe } from "../../hardcodedData/fullRecipe";
 
 export default function Recipe() {
   const router = useRouter();
-  const recipe: FullRecipeType = userRecipeHardcoded;
+  const recipe: FullRecipeType = userRecipe;
   // <p>Recipe id: {router.query.id}</p>
 
   return (
     <Layout>
-      <FullRecipeView recipe={recipe} />
+      <RecipeView recipe={recipe} />
     </Layout>
   );
 }

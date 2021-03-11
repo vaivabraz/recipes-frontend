1138ca6b379eb233a7cf139bfb3b805514060138
vabr@danskebank.lt <vabr@danskebank.lt>
2021-03-11 20:17:21 +0200

    adjusted style on small screens

--- src/pagesComponents/CreateNewRecipeView/ingredients/IngredientLine.tsx
+++ src/pagesComponents/CreateNewRecipeView/ingredients/IngredientLine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { IngredientsListItemType } from "../../../types";
-import { InputStyle, ButtonSvg } from "../../../ui";
+import { InputStyle, ButtonSvg, Row } from "../../../ui";
 
 import MinusButton from "../../../asset/svg/MinusButton";
 
@@ -31,32 +31,34 @@ const IngredientLine = ({ entry, onChange, onRemove }: Props) => {
         name={"product"}
         autoComplete="off"
       />
-      <QuantityInput
-        placeholder="Kiekis"
-        value={entry.quantity}
-        onChange={onInputChange}
-        name={"quantity"}
-        autoComplete="off"
-      />
-      <ButtonSvg onClick={onRemoveProduct} Icon={MinusButton} />
+      <>
+        <QuantityInput
+          placeholder="Kiekis"
+          value={entry.quantity}
+          onChange={onInputChange}
+          name={"quantity"}
+          autoComplete="off"
+        />
+        <ButtonSvg onClick={onRemoveProduct} Icon={MinusButton} />
+      </>
     </Line>
   );
 };
 
 export default React.memo(IngredientLine);
 
-const Line = styled.div`
-  display: flex;
-  margin: 0.5rem 1rem;
+const Line = styled(Row)`
+  margin: 0 0.5rem;
 `;
 
 const ProductInput = styled.input`
   ${InputStyle}
   flex: 1;
+  margin: 0.5rem 0;
 `;
 
 const QuantityInput = styled.input`
   ${InputStyle}
   width: 10rem;
-  margin: 0 0.5rem;
+  margin: 0.5rem;
 `;

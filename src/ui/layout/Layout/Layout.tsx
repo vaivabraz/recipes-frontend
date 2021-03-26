3727339eb3d00f4d4f9d3d898ab82b0372243d87
vabr@danskebank.lt <vabr@danskebank.lt>
2021-03-26 21:07:13 +0200

    layout component with authorization check

--- src/ui/layout/Layout/Layout.tsx
+++ src/ui/layout/Layout/Layout.tsx
@@ -1,19 +1,35 @@
+import React, { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 import Navbar from "./Navbar";
 import SubMenu from "./SubMenu";
-import styled from "styled-components";
 import { Colors } from "../../../constants";
 import { VSpace, Column } from "../";
+import { AppContext } from "../../../store";
 
-const Layout = (props) => (
-  <LayoutContainer>
-    <Navbar />
-    <SubMenu />
-    <VSpace />
-    <Column large={10}>{props.children}</Column>
-    <VSpace />
-  </LayoutContainer>
-);
+const Layout = ({ children }) => {
+  const { state } = useContext(AppContext);
+  const router = useRouter();
 
+  //TODO:if state.loading -> show loading overlay
+
+  useEffect(() => {
+    if (!state.user.token) {
+      router.push("/login");
+      return;
+    }
+  }, [state.user.token]);
+
+  return (
+    <LayoutContainer>
+      <Navbar />
+      <SubMenu />
+      <VSpace />
+      <Column large={10}>{children}</Column>
+      <VSpace />
+    </LayoutContainer>
+  );
+};
 const LayoutContainer = styled(Column)`
   background-color: ${Colors.Background};
   align-items: center;

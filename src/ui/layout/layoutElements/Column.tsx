26eec858c2aabea8257f743086eabee69be9f31c
vabr@danskebank.lt <vabr@danskebank.lt>
2021-04-07 20:59:03 +0300

    optional responsive param

--- src/ui/layout/layoutElements/Column.tsx
+++ src/ui/layout/layoutElements/Column.tsx
@@ -11,7 +11,7 @@ type Props = {
   offsetMedium?: ColumnOffset;
   offsetLarge?: ColumnOffset;
   style?: object;
-  responsive: boolean;
+  responsive?: boolean;
 };
 
 export type ColumnSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;

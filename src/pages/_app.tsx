1b7d19e2bf2c7697fd9bc280005d044b52d664fb
Vaiva Brazaityte <bd5844@fvfgk53vq05p.dk10.danskenet.net>
2022-01-06 14:19:50 +0200

    moved mui theme from app.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,63 +1,12 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import GlobalStyles from "../ui/styles/global";
 import { AppProvider } from "../store/context";
 import { WithAxios } from "../services/axios";
+import { muiTheme } from "../utils";
 
 export default function MyApp({ Component, pageProps }) {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#7D4F50",
-      },
-      secondary: {
-        main: "#7D4F50",
-      },
-    },
-    typography: {
-      allVariants: {
-        letterSpacing: "0.1em",
-      },
-      h1: {
-        fontSize: "39.06px",
-      },
-      h2: {
-        fontSize: "31.25px",
-      },
-      h3: {
-        fontSize: 25,
-      },
-      h4: {
-        fontSize: "20px",
-      },
-      h5: {
-        fontSize: "16px",
-        lineHeight: "24px",
-      },
-      h6: {
-        fontSize: "24px",
-      },
-    },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            borderRadius: 12,
-          },
-        },
-      },
-      MuiOutlinedInput: {
-        styleOverrides: {
-          root: {
-            borderRadius: 12,
-            backgroundColor: "#ffffff33",
-          },
-        },
-      },
-    },
-  });
-
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -70,7 +19,7 @@ export default function MyApp({ Component, pageProps }) {
     <AppProvider>
       <WithAxios>
         <GlobalStyles />
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={muiTheme}>
           <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
           </QueryClientProvider>

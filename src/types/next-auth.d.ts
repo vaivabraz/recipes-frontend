535122c2b7f355776cb980d1196b58748e372de2
Vaiva B <vaiva.brazaityte@unrvld.com>
2022-11-10 12:15:23 +0200

    next-auth types

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+        username: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    username: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    username?: string
+  }
+}
